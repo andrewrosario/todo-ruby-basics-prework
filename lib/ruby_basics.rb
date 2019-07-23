f038681ba1fce9a2ca9293e5b69bec1fe9b084ef
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-23 20:17:40 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(value)
+  return value
 end
 
-def greeting
+def greeting(name, language)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
